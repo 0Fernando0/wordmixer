d403fd32d8dd427ce270125e35d806a25b20ea5a
0Fernando0 <fernandoborgespereira57@gmail.com>
2024-09-23 18:07:54 -0300

    feat: add logic.js

--- logic.js
+++ logic.js
@@ -1,17 +1,83 @@
 let tentativas = 1
 
-function setTentativa(tentativa){
-    document.getElementById("tentativas").textContent = `${tentativa}/10`
+const PALAVRAS = [
+    "terra",
+    "agua",
+    "fogo",
+    "vento"
+];
+
+palavra_correta = null;
+user = document.getElementById("user");
+score = document.getElementById("score");
+
+document.body.onload = function(){
+    carregarPalavra();
+}
+
+function endGameScreen(){
+    let mensagem = "Fim De Jogo\n" + `Pontuação: ${score.textContent}`
+    alert(mensagem);
+}
+
+function acertar(){
+    carregarPalavra()
+    score.textContent = Number(score.textContent) + 20; 
+    user.value = "";
+    user.focus();
+}
+
+function errar(){
+    alert("errou");
+    setTentativa(++tentativas);
+}
+
+function tentar(){
+    if(user.value == palavra_correta){ acertar() }
+    else{ errar() }
 }
 
 
-function pular(){
-    if(tentativas == 10){
-        alert("Fim De Jogo");
-        tentativas = 1;
-        setTentativa(tentativas);
-        return;
+function shuffle(o) {
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  }
+
+function embaralhar(palavra){
+    let palavra_embaralhada = "";
+    let array_palavra = String(palavra).split("");
+    let array_embaralhado = shuffle(array_palavra);
+
+    for (const item of array_embaralhado) {
+        palavra_embaralhada += item;
     }
-    tentativas++;
-    setTentativa(tentativas)
+    return palavra_embaralhada;
+}
+
+function carregarPalavra(){
+    let random = PALAVRAS[Math.floor(Math.random() * PALAVRAS.length)];
+    palavra_correta = random;
+    let palavra_embaralhada = embaralhar(random);
+    document.getElementById("palavra").textContent = palavra_embaralhada;
+}
+
+
+function zerar(){
+    endGameScreen();
+    score.textContent = 0;
+    tentativas = 1;
+    document.getElementById("tentativas").textContent = 1;
+    carregarPalavra()
+}
+
+function setTentativa(tentativa){
+    if(tentativa > 10){ zerar() }
+    else{
+        document.getElementById("tentativas").textContent = tentativa;
+    }
+}
+
+function pular(){
+    setTentativa(++tentativas)
+    carregarPalavra()
 }
